5383527ed6c38efb9e2cf25157d7e533f4f9799f
nathanhall762 <nathanhall762@gmail.com>
2023-09-05 10:50:51 -0500

    init and task 0

---
+++ task_0/src/index.js
@@ -0,0 +1,9 @@
+// Importing jQuery
+import $ from 'jquery';
+
+// Adding three different paragraphs to the page body
+$(function() {
+  $('body').append('<p>This is paragraph 1.</p>');
+  $('body').append('<p>This is paragraph 2.</p>');
+  $('body').append('<p>This is paragraph 3.</p>');
+});
