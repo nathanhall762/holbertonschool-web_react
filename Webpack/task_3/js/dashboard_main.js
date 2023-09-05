5ea5e14aaf2a70e3c89b3a256ad01d7b3bf6a3ae
nathanhall762 <nathanhall762@gmail.com>
2023-09-05 13:56:16 -0500

    task3 init

---
+++ Webpack/task_3/js/dashboard_main.js
@@ -0,0 +1,41 @@
+// Importing jQuery
+import $ from "jquery";
+// Importing lodash as _
+import _ from "lodash";
+// Importing main.css
+import "../css/main.css";
+
+// Variable to keep track of button clicks
+var count = 0;
+
+// Function to update the counter
+function updateCounter() {
+  count += 1;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+// Function to add elements
+function addElements() {
+  // Adding paragraph elements
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+
+  // Adding button element
+  $("body").append(
+    '<button id="clickButton">Click here to get started</button>'
+  );
+
+  // Adding paragraph element with id='count'
+  $("body").append('<p id="count"></p>');
+
+  // Adding another paragraph
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // Binding the debounce function to the click event using .on()
+  $("#clickButton").on("click", _.debounce(updateCounter, 500));
+}
+
+// Adding elements when the document is ready
+$(function () {
+  addElements();
+});
--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,16 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // Set mode to development
+  mode: 'production', // Set mode to production
   entry: './js/dashboard_main.js', // Entry point of your application
   output: {
     filename: 'bundle.js', // Output filename
-    path: path.resolve(__dirname, 'public'), // Output directory
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'), // Serve content from the public directory
-    port: 8564, // Port to run the dev server on
-    open: true, // Open browser automatically
+    path: path.resolve(__dirname, 'public') // Output directory
   },
   module: {
     rules: [
@@ -18,8 +13,8 @@ module.exports = {
         test: /\.css$/, // Test for CSS files
         use: [
           'style-loader', // Injects styles into DOM
-          'css-loader', // Translates CSS into CommonJS
-        ],
+          'css-loader' // Translates CSS into CommonJS
+        ]
       },
       {
         test: /\.(png|jpg|gif|svg)$/, // Test for image files
@@ -28,17 +23,17 @@ module.exports = {
             loader: 'file-loader', // Handles file imports and emits them as files
             options: {
               name: '[name].[ext]',
-              outputPath: 'images/', // Output path for images
-            },
+              outputPath: 'images/' // Output path for images
+            }
           },
           {
             loader: 'image-webpack-loader', // Optimize images
             options: {
-              bypassOnDebug: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
+              bypassOnDebug: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
