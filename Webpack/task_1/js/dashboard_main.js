cbc57b529266103486043ccdede2da500704a11c
nathanhall762 <nathanhall762@gmail.com>
2023-09-05 12:50:38 -0500

    task1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,39 @@
+// Importing jQuery
+import $ from "jquery";
+// Importing debounce function from lodash
+import { debounce } from "lodash";
+
+// Variable to keep track of button clicks
+let count = 0;
+
+// Function to update the counter
+function updateCounter() {
+  count += 1;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+// Function to add elements
+function addElements() {
+  // Adding paragraph elements
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+
+  // Adding button element
+  $("body").append(
+    '<button id="clickButton">Click here to get started</button>'
+  );
+
+  // Adding paragraph element with id='count'
+  $("body").append('<p id="count"></p>');
+
+  // Adding another paragraph
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // Binding the debounce function to the click event using .on()
+  $("#clickButton").on("click", debounce(updateCounter, 300));
+}
+
+// Adding elements when document is ready
+$(function () {
+  addElements();
+});
---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // Set mode to production
+  entry: './js/dashboard_main.js', // Entry point of your application (change this path if needed)
+  output: {
+    filename: 'bundle.js', // Output filename
+    path: path.resolve(__dirname, 'public') // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Test for JavaScript files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: 'babel-loader' // Use babel-loader to transpile JavaScript
+        }
+      }
+    ]
+  }
+};
